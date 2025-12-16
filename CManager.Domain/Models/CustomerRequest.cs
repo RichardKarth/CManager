8e047d5e97277192448166286443a2894ea0cc19
Richard Karth <richard.k.triumph@gmail.com>
2025-12-16 14:14:50 +0100

    Created the code for customer, requests and exceptions

--- CManager.Domain/Models/CustomerRequest.cs
+++ CManager.Domain/Models/CustomerRequest.cs
@@ -1,7 +1,11 @@
 ﻿
 namespace CManager.Domain.Models
 {
-    public class CustomerRequest : Customer
+    public class CustomerRequest
     {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; }
     }
 }
